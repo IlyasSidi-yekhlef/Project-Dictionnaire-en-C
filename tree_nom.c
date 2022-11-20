a92568214eee786c3ac7c8039c33291b20327861
IlyasSidi-yekhlef <100586418+IlyasSidi-yekhlef@users.noreply.github.com>
2022-11-20 02:21:18 +0100

    coucou

--- tree_nom.c
+++ tree_nom.c
@@ -13,10 +13,6 @@ p_tree_nom fill_tree_nom(){
     p_nom_node temp = NULL;
     int count = 1;
     char end = '\0';
-//    int i;
-//    p_nom_node temp = simple_tree.root;
-//    temp = (p_nom_node) malloc(sizeof(t_nom_node));
-//    temp =simple_tree.root;
 
     FILE *dicofile =fopen("C:\\Users\\sidiy\\CLionProjects\\PRojet_DicOReel\\dictio.txt", "rt"); //mettre true path (clique droit sur le fichier copy path reference puis absolute reference car C part de la racine)
     char flechie[35];
@@ -35,9 +31,6 @@ p_tree_nom fill_tree_nom(){
                 }
                 temp = simple_tree.root[i];
                 while (base[count] != end) {
-//                for (i = 1; i >= count; i++) {
-//                    temp = temp->sons[base[i] - 97];
-//                }
 
                     if(base[count] == '-'){
                         i = 26;
@@ -45,10 +38,7 @@ p_tree_nom fill_tree_nom(){
                     else{
                         i = base[count] - 97;
                     }
-//                printf("%d",temp->sons[i]->letter);
-//                if(count != 1){
-//                    temp = temp->sons[i];
-//                }
+                    printf("%d",i);
 
                     if (temp->sons[i] == NULL && base[count] != end) {
                         printf("/OK/");
@@ -60,73 +50,22 @@ p_tree_nom fill_tree_nom(){
 //                display_tree_nom(simple_tree);
                 }
                 printf("fin\n");
-                i = base[0] - 97;
+                for(int a = 0;a<=35;a++){
+                    temp->flechie[a] = flechie[a];
+                    temp->base[a] = base[a];
+                }
+                temp->nb = 1;
+                
 
                 count = 1;
             }
-//            i = base[0] - 97;
-//            printf("base : %s\n",base);
-//            printf("emplacement : %d\n",i);
-//            if( simple_tree.root[i] == NULL && base[0] != end){
-//                simple_tree.root[i] = creat_node_nom(base[0]);
-//            }
-//            temp = simple_tree.root[i];
-//            while (base[count] != end) {
-////                for (i = 1; i >= count; i++) {
-////                    temp = temp->sons[base[i] - 97];
-////                }
-//                i = base[count] - 97;
-////                printf("%d",temp->sons[i]->letter);
-////                if(count != 1){
-////                    temp = temp->sons[i];
-////                }
-//
-//                if (temp->sons[i] == NULL && base[count] != end) {
-//                    printf("/OK/");
-//                    temp->sons[i] = creat_node_nom(base[count]);
-//                }
-//                temp = temp->sons[i];
-//                count++;
-//                printf("/%d\n",count);
-////                display_tree_nom(simple_tree);
-//            }
-//            printf("fin\n");
-//            i = base[0] - 97;
-//
-//            count = 1;
 
 
         }
         fclose(dicofile);
     } return simple_tree;
-    printf("l");
-    return simple_tree;
 }
 
-
-//void fill_nodes_nom(p_tree_nom simple_tree, char base[35]) {
-//    int i;
-//    p_nom_node temp;
-//    temp = simple_tree.root[base[0]];
-//    int count = 0;
-//    char l = base[count];
-//    while(count != 35){
-//        printf("%c",base[count]);
-//        count++;
-//    }
-//    while (l != "b") {
-//        for (i = 0; i >= count; i++) {
-//            temp = temp->sons[base[i] - 97];
-//            }
-//        if (temp != NULL) {
-//            temp = creat_node_nom(base[count]);
-//            }
-//        count++;
-//        l = base[count];
-//        }
-//
-//
-//    }
 
 
 void display_tree_nom(t_tree_nom simple_tree){
